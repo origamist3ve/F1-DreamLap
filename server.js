5c8246910cb8dd379c13be3400e416b617a9a09f
origamist3ve <sb63972n@pace.edu>
2025-04-01 11:17:10 -0400

    established a connection with mongoDB

--- server.js
+++ server.js
@@ -6,3 +6,32 @@ import methodOverride from "method-override";
 import morgan from "morgan";
 import session from "express-session";
 import path from "path";
+
+import "./db/connections.js"
+
+// import autController from "./controllers/auth.js"
+import {fileURLToPath} from "url";
+
+const port = process.env.PORT ? process.env.PORT : '3000';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(__dirname));
+
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
